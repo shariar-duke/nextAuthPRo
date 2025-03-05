505f965e2e8c40e8a46a5ddbaf1f0db56df10d32
Shariar Duke <shariarmahmud225@gmail.com>
2025-03-05 12:38:42 +0600

    srver action emplemented

--- src/app/actions/index.js
+++ src/app/actions/index.js
@@ -1,6 +1,8 @@
 "use server";
+import { signIn } from "../../../auth";
 export async function doSocialLogin(formData) {
   const action = formData.get("action");
+  await signIn(action, { redirectTo: "/home" });
   console.log(action);
 }
 
