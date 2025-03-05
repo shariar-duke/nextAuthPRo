6f5d17325d570156c3a2807e76b547bfa7121c54
Shariar Duke <shariarmahmud225@gmail.com>
2025-03-05 10:36:56 +0600

    next auth configuration for google only

---
+++ auth.js
@@ -0,0 +1,21 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+    GithubProvider({}),
+  ],
+});
