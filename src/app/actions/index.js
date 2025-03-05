25e5b9b45f990cf011d8d6a5eaefd8c9cbd5d7b1
Shariar Duke <shariarmahmud225@gmail.com>
2025-03-05 10:14:50 +0600

    button value reterived

--- src/app/actions/index.js
+++ src/app/actions/index.js
@@ -1,3 +1,7 @@
-export async function doSocialLogin(formData) {}
+"use server";
+export async function doSocialLogin(formData) {
+  const action = formData.get("action");
+  console.log(action);
+}
 
 export async function doLogout() {}
