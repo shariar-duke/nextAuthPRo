f38910d8cd9d673e1c44cba65dcb2787b13a10b1
Shariar Duke <shariarmahmud225@gmail.com>
2025-03-06 14:17:08 +0600

    LoginForm updated with going to signup option

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,5 +1,6 @@
 "use client";
 import { doCradentialLogin } from "@/app/actions";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import SocialLogin from "./SocialLogin";
@@ -27,8 +28,8 @@ export default function LoginForm() {
   };
 
   return (
-    <div className="max-w-md mx-auto p-4 border border-gray-300 rounded-lg shadow-md">
-      <form onSubmit={handleSubmit} className="space-y-4">
+    <div className="max-w-lg mx-auto p-6 border border-gray-300 rounded-lg shadow-md">
+      <form onSubmit={handleSubmit} className="space-y-5">
         <div>
           <label
             htmlFor="email"
@@ -42,7 +43,7 @@ export default function LoginForm() {
             name="email"
             placeholder="Enter your email"
             required
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+            className="mt-1 block w-full px-4 py-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
           />
         </div>
 
@@ -59,20 +60,16 @@ export default function LoginForm() {
             name="password"
             placeholder="Enter your password"
             required
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+            className="mt-1 block w-full px-4 py-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
           />
         </div>
 
-        {error && (
-          <div className="text-red-500 text-sm mt-2">
-            {error} {/* Display error message here */}
-          </div>
-        )}
+        {error && <div className="text-red-500 text-sm mt-2">{error}</div>}
 
         <div>
           <button
             type="submit"
-            className="w-full py-2 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="w-full py-3 bg-blue-500 text-white text-lg font-semibold rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
           >
             Credential Login
           </button>
@@ -83,6 +80,14 @@ export default function LoginForm() {
       <div className="mt-6">
         <SocialLogin />
       </div>
+
+      <p className="text-center text-lg mt-4">Don&apos;t have an account?</p>
+      <Link
+        href="/register"
+        className="block text-lg text-blue-600 underline text-center mt-2 hover:text-blue-700"
+      >
+        Register
+      </Link>
     </div>
   );
 }
