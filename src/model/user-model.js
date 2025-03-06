b437a3309ba0d8d67b3db3398547247e6db245e5
Shariar Duke <shariarmahmud225@gmail.com>
2025-03-06 23:15:28 +0600

    user model and schema created

---
+++ src/model/user-model.js
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: {
+      required: true,
+      type: String,
+    },
+    password: {
+      required: true,
+      type: String,
+    },
+    email: {
+      required: true,
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
