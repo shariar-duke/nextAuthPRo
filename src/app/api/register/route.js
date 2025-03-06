a5640bf4ad6613123638407dd5f6cf93d2eb49f0
Shariar Duke <shariarmahmud225@gmail.com>
2025-03-06 18:43:16 +0600

    route.js initialization

---
+++ src/app/api/register/route.js
@@ -0,0 +1,11 @@
+export const POST = async (request) => {
+  const { name, email, password } = await request.json();
+
+  // create here a mongodb data connection
+
+  // Encrypt  the password
+
+  // Form a Db payload
+
+  // Update the Db
+};
