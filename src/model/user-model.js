ada1f41240fc49edf76dd87501773613f8680a22
Shariar Duke <shariarmahmud225@gmail.com>
2025-03-07 09:04:00 +0600

    user modell update

--- src/model/user-model.js
+++ src/model/user-model.js
@@ -18,4 +18,4 @@ const userSchema = new Schema(
   { timestamps: true }
 );
 
-export const User = mongoose.model("User", userSchema);
+export const User = mongoose.models.User ?? mongoose.model("User", userSchema);
