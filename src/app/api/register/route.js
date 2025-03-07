7cc45e458a0d1d888ac11a6f967bdb45bf6e347c
Shariar Duke <shariarmahmud225@gmail.com>
2025-03-07 09:02:44 +0600

    api route compplete for data post

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,3 +1,6 @@
+import { dbConnect } from "@/lib/mongo.";
+import { createUser } from "@/queries/users";
+import bcrypt from "bcryptjs";
 import { NextResponse } from "next/server";
 export const POST = async (request) => {
   const { name, email, password } = await request.json();
@@ -5,12 +8,25 @@ export const POST = async (request) => {
   console.log(name, email, password);
 
   // create here a mongodb data connection
-
+  await dbConnect();
   // Encrypt  the password
+  const hashedPassword = await bcrypt.hash(password, 5);
 
   // Form a Db payload
 
+  const newUser = {
+    name,
+    email,
+    password: hashedPassword,
+  };
   // Update the Db
+  try {
+    await createUser(newUser);
+  } catch (err) {
+    return new NextResponse(err.message, {
+      status: 500,
+    });
+  }
 
   return new NextResponse("User has been Created", {
     status: 201,
