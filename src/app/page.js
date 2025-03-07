0b840de47f9bceb914517fba29b51525dad032bf
Shariar Duke <shariarmahmud225@gmail.com>
2025-03-07 10:59:51 +0600

    root page updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
-import LoginForm from "@/components/LoginForm";
+import Link from "next/link";
 
-export default function Home() {
+export default function Landing() {
   return (
-    <div className="flex flex-col justify-center items-center m-4">
-      <h1 className="text-3xl my-3">Hey time to sign In</h1>
-      <LoginForm />
-    </div>
+    <>
+      <div>Landing Page</div>
+      <div className="flex flex-col justify-center items-center p-8">
+        <Link className="text-3xl underline" href="/products">
+          All Products
+        </Link>
+      </div>
+    </>
   );
 }
