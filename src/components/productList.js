074522e0ca649879bdfcb7d9ebd44dfcaa2ad923
Shariar Duke <shariarmahmud225@gmail.com>
2025-03-07 10:48:29 +0600

    productlist component

---
+++ src/components/productList.js
@@ -0,0 +1,21 @@
+import { getAllProducts } from "@/data/products";
+import Link from "next/link";
+
+const ProductList = () => {
+  const products = getAllProducts();
+  return (
+    <div className="flex flex-col justify-center items-center p-8">
+      {products.map((product) => (
+        <Link key={product.id} href={`/products/${product.id}`}>
+          <div className="text-2xl">
+            <p>
+              {product.image} - {product.name}
+            </p>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default ProductList;
