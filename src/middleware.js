3889e233267627894d4d420f8031b770c6685383
Shariar Duke <shariarmahmud225@gmail.com>
2025-03-07 13:16:47 +0600

    middleware modified

--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,12 @@
+import NextAuth from "next-auth";
 import { NextResponse } from "next/server";
+import { authConfig } from "./auth.config";
 
-export function middleware(request) {
+const { auth } = NextAuth(authConfig);
+
+export async function middleware(request) {
+  const session = await auth();
+  console.log(session);
   return NextResponse.redirect(new URL("/", request.url));
 }
 
